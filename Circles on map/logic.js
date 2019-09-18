2b0a6a81bc453a12304c184b7d2b75a1479a3aff
gfullsack <gfullsack@gmail.com>
2019-09-17 21:18:21 -0700

    Map update

--- Circles on map/logic.js
+++ Circles on map/logic.js
@@ -18,30 +18,68 @@ function markerSize(population) {
 
 // Each city object contains the city's name, location and population
 // Import Data
-d3.csv("Alcoholic_Beverages_df.csv")
+d3.csv("Clean_Alcoholic_Beverages_df.csv")
   .then(function(alcohol){
 
 // Step 1: Parse Data/Cast as numbers
     // ==============================
+    var Country_List = [] 
+    var Country_Tonnes = [] 
+    var location = []
     alcohol.forEach(function(data) {
-      // data.Area =+data.Area
-      data.latitude = +data.latitude;
-      data.longitude = +data.longitude;
-      data.Y1961 = +data.Y1961;
+      Country_List.push(data.Area);
+      // console.log(Country_List);
+      data.latitude = (+data.latitude);
+      data.longitude = (+data.longitude);
+      var coordinates = [data.latitude, data.longitude];
+      location.push(coordinates);
+      Country_Tonnes.push(+data.Y1961);
     });
 // console.log(alcohol);
 
 
 // Loop through the cities array and create one marker for each city object
-for (var i = 0; i < cities.length; i++) {
-  L.circle(cities[i].location, {
+for (var i = 0; i < Country_List.length; i++) {
+  L.circle(location[i], { // <- CHANGED THIS
     fillOpacity: 0.75,
     color: "white",
     fillColor: "purple",
     // Setting our circle's radius equal to the output of our markerSize function
-    // This will make our marker's size proportionate to its population
-    radius: markerSize(cities[i].population)
-  }).bindPopup("<h1>" + cities[i].name + "</h1> <hr> <h3>Population: " + cities[i].population + "</h3>").addTo(myMap);
+    // This will make our marker's size proportionate to its Tonnes
+    radius: 1 + Country_Tonnes[i] * 100 // <- CHANGED THIS (took out markerSize)
+  }).bindPopup("<h1>" + Country_List[i] + "</h1> <hr> <h3> Tonnes of Alcohol: " + Country_Tonnes[i] + "</h3>").addTo(myMap);
 }
+
+
+function init() {
+
+
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  d3.json("/names").then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+};
+
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildCharts(newSample);
+  buildMetadata(newSample);
+};
+
+// Initialize the dashboard
+init();
 
   });
