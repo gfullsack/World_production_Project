f3ab65a79fd4c0efa4d85d0b258b9d085d1cd2cf
Solene Feuillu <solene.feuillu@gmail.com>
2019-09-17 18:31:17 -0700

    circles on map
    using example world cup and hair to generate circles on map_ no finished_WIP

---
+++ Circles on map/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoic29sZW5lZiIsImEiOiJjazA3azFlejEwODJ6M25wcWF0ZHpwNDc2In0.Sr0ofAx9JxDuIIHvQXSQ0A";
---
+++ Circles on map/logic.js
@@ -0,0 +1,47 @@
+// Create a map object
+var myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 5
+});
+
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets-basic",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// Define a markerSize function that will give each city a different radius based on its population
+function markerSize(population) {
+  return population / 40;
+}
+
+// Each city object contains the city's name, location and population
+// Import Data
+d3.csv("Alcoholic_Beverages_df.csv")
+  .then(function(alcohol){
+
+// Step 1: Parse Data/Cast as numbers
+    // ==============================
+    alcohol.forEach(function(data) {
+      // data.Area =+data.Area
+      data.latitude = +data.latitude;
+      data.longitude = +data.longitude;
+      data.Y1961 = +data.Y1961;
+    });
+// console.log(alcohol);
+
+
+// Loop through the cities array and create one marker for each city object
+for (var i = 0; i < cities.length; i++) {
+  L.circle(cities[i].location, {
+    fillOpacity: 0.75,
+    color: "white",
+    fillColor: "purple",
+    // Setting our circle's radius equal to the output of our markerSize function
+    // This will make our marker's size proportionate to its population
+    radius: markerSize(cities[i].population)
+  }).bindPopup("<h1>" + cities[i].name + "</h1> <hr> <h3>Population: " + cities[i].population + "</h3>").addTo(myMap);
+}
+
+  });
